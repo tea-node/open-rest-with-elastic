b6dd60704ec512074bd28e711d24d8bc1fd62fb9
baiyu <baiyu@admaster.com.cn>
2019-11-25 23:35:34 +0800

    update

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ const Init = (rest, path) => {
   /** 释放 ElasticSearch 和 Client */
   rest.ElasticSearch = ElasticSearch;
 
+  /** 添加define方法 */
+  rest.ElasticSearch.Client.prototype.define = function (modelName, attributes, options) {
+    const opts = options || {};
+    opts.client = this;
+    opts.modelName = modelName;
+    const model = new Model(modelName, attributes, opts).init();
+    return model;
+  };
+
   /**
     * model 的 初始化
     * 将获取Model类的方法注册到rest.utils上
